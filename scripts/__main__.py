27637644fb2c3ad972a92e833dac469522147087
Alejandro Piad <alepiad@gmail.com>
2020-06-14 11:27:53 +0200

    Extract relations for corpus

--- scripts/__main__.py
+++ scripts/__main__.py
@@ -27,5 +27,19 @@ def extract_corpus_entities():
         print(e)
 
 
+def extract_corpus_relations():
+    corpus = load_training_data("cord19")
+
+    relations = set()
+
+    for sentence in corpus.sentences:
+        for relation in sentence.relations:
+            relations.add((relation.from_phrase.text, relation.label, relation.to_phrase.text))
+
+    for e in sorted(relations):
+        print("\t".join(e))
+
+
+
 if __name__ == "__main__":
     fire.Fire()
