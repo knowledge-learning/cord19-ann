4b2d73d8659dad29bd890c1bbffcca5c51ddd741
Houcemeddine Turki <turkiabdelwaheb@hotmail.fr>
2020-07-31 17:28:48 +0200

    Update entities.py

--- data/output/wdreconciliation/entities.py
+++ data/output/wdreconciliation/entities.py
@@ -17,11 +17,21 @@ with open(r'C:\Users\Houcemeddine Turki\Downloads\entities11.tsv', newline='') a
          print(row[0])
          endpoint_url = "https://query.wikidata.org/sparql"
          query = '''SELECT DISTINCT ?item (UCASE(?label) AS ?title) WHERE {
-            {?item wdt:P2892 [].} UNION {?item wdt:P6680 [].} UNION {?item wdt:P351 []; wdt:P703 wd:Q15978631.}
-            {?item rdfs:label ?label} UNION { ?item skos:altLabel ?label. }
-            FILTER(LANG(?label)="en")
-            FILTER(STRSTARTS(UCASE(?label),UCASE("'''+row[0]+'''")))
-         }'''
+         { ?item wdt:P2892 []. }
+         UNION
+         { ?item wdt:P6694 []. }
+         UNION
+         {
+          ?item wdt:P351 [];
+                wdt:P703 wd:Q15978631.
+         }
+         { ?item rdfs:label ?label. }
+         UNION
+         { ?item skos:altLabel ?label. }
+         FILTER((LANG(?label)) = "en")
+         FILTER(STRSTARTS(UCASE(?label), UCASE("'''+row[0]+'''")))
+         }
+         ORDER BY (?title)'''
          print(query)
          def get_results(endpoint_url, query):
              user_agent = "WDQS-example Python/%s.%s" % (sys.version_info[0], sys.version_info[1])
@@ -36,10 +46,12 @@ with open(r'C:\Users\Houcemeddine Turki\Downloads\entities11.tsv', newline='') a
              sheet.write(row1,0,row[0])
              try:
                  sheet.write(row1,1,result["item"]["value"])
+                 print(result["item"]["value"])
              except KeyError:
                  sheet.write(row1,1,"KeyError")
              try:
                  sheet.write(row1,2,result["label"]["value"])
+                 print(result["label"]["value"])
              except KeyError:
                  sheet.write(row1,2,"KeyError")
              row1 += 1
