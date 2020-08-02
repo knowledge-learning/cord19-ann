1fc645730c7e62981cdd726b752475e8925dda28
Suilan Estevez <sestevez@titan.dlsi.ua.es>
2020-08-02 17:40:43 +0200

    Saving after each query

--- data/output/wdreconciliation/entities.py
+++ data/output/wdreconciliation/entities.py
@@ -6,63 +6,60 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 import csv
 import xlsxwriter
 
-w = xlsxwriter.Workbook("wdentities.xlsx")
-sheet = w.add_worksheet("alignment")
-
 row1 = 0
 
-with open('entities11.tsv', newline='') as csvfile:
-     spamreader = csv.reader(csvfile, delimiter='\t', quotechar='|')
-     for row in spamreader:
-         print(row[0])
-         endpoint_url = "https://query.wikidata.org/sparql"
-         query = '''SELECT DISTINCT ?item (UCASE(?label) AS ?title) WHERE {
-         { ?item wdt:P2892 []. }
-         UNION
-         { ?item wdt:P6694 []. }
-         UNION
-         {
-          ?item wdt:P351 [];
-                wdt:P703 wd:Q15978631.
-         }
-         { ?item rdfs:label ?label. }
-         UNION
-         { ?item skos:altLabel ?label. }
-         FILTER((LANG(?label)) = "en")
-         FILTER(STRSTARTS(UCASE(?label), UCASE("'''+row[0]+'''")))
-         }
-         ORDER BY (?title)'''
-         print(query)
-         def get_results(endpoint_url, query):
-             user_agent = "WDQS-example Python/%s.%s" % (sys.version_info[0], sys.version_info[1])
-             # TODO adjust user agent; see https://w.wiki/CX6
-             sparql = SPARQLWrapper(endpoint_url, agent=user_agent)
-             sparql.setQuery(query)
-             sparql.setReturnFormat(JSON)
-             return sparql.query().convert()
-         if (len(row[0])>2):
-             try:
-               results = get_results(endpoint_url, query)
-               e = 1
-             except:
-               print("EndPointInternalError")
-               e = 0
-           n = 0
-             if e!=0:
-                 for result in results["results"]["bindings"]:
-                     sheet.write(row1,0,row[0])
-                     try:
-                         sheet.write(row1,1,result["item"]["value"])
-                         print(result["item"]["value"])
-                     except KeyError:
-                         sheet.write(row1,1,"KeyError")
-                     try:
-                         sheet.write(row1,2,result["label"]["value"])
-                         print(result["label"]["value"])
-                     except KeyError:
-                         sheet.write(row1,2,"KeyError")
-                 row1 += 1
-             n += 1
-             if n == 4: break
- 
-w.close()
+with open("result.csv", "w") as writer:
+    with open(sys.argv[1], newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter='\t', quotechar='|')
+        for row in spamreader:
+            entity = row[0].strip('"(),.;')
+            print(entity)
+            endpoint_url = "https://query.wikidata.org/sparql"
+            query = '''SELECT DISTINCT ?item (UCASE(?label) AS ?title) WHERE {
+            { ?item wdt:P2892 []. }
+            UNION
+            { ?item wdt:P6694 []. }
+            UNION
+            {
+            ?item wdt:P351 [];
+                    wdt:P703 wd:Q15978631.
+            }
+            { ?item rdfs:label ?label. }
+            UNION
+            { ?item skos:altLabel ?label. }
+            FILTER((LANG(?label)) = "en")
+            FILTER(STRSTARTS(UCASE(?label), UCASE("'''+entity+'''")))
+            }
+            ORDER BY (?title)'''
+            print(query)
+            def get_results(endpoint_url, query):
+                user_agent = "WDQS-example Python/%s.%s" % (sys.version_info[0], sys.version_info[1])
+                # TODO adjust user agent; see https://w.wiki/CX6
+                sparql = SPARQLWrapper(endpoint_url, agent=user_agent)
+                sparql.setQuery(query)
+                sparql.setReturnFormat(JSON)
+                return sparql.query().convert()
+            if (len(entity)>2):
+                try:
+                    results = get_results(endpoint_url, query)
+                    e = 1
+                except KeyboardInterrupt:
+                    raise
+                except:
+                    print("EndPointInternalError")
+                    continue
+
+                for result in results["results"]["bindings"]:
+                    writer.write(f"{row1},0,{entity}\n")
+                    try:
+                        writer.write(f"{row1},1,{result['item']['value']}\n")
+                        print(result["item"]["value"])
+                    except KeyError:
+                        writer.write(f"{row1},1,KeyError\n")
+                    try:
+                        writer.write(f"{row1},2,{result['label']['value']}\n")
+                        print(result["label"]["value"])
+                    except KeyError:
+                        writer.write(f"{row1},2,KeyError\n")
+                    row1 += 1
+                    writer.flush()
